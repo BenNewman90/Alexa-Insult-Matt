b9a5f062a21756eb482f22cd23de1e2347178c30
DenisKent <kentdenis6@hotmail.co.uk>
2018-07-16 00:41:46 +0300

    new text edits

--- app.js
+++ app.js
@@ -21,31 +21,45 @@ var insults = [
 
   "Matt, or shall I call you AbuMustafa. You're not a local, shut up and sit down.",
 
-  "Matt, you are always going on about how client facing you are. Do you want to face some hard truths? You're a cunt!",
+  "You are always going on about how client facing you are. Do you want to face some hard truths? You're a cunt!",
 
-  "Matt, sarcasm is the lowest form of humour...Except for your shit puns, they are definitely worse.",
+  "Sarcasm is the lowest form of humour...Except for your shit puns, they are definitely worse.",
 
-  "Matt, you are weak as shit. Sol said that you are the weakest boy in the class and that she would definitely own you in an arm wrestle. You're pathetic. Insect.",
+  "You are weak as shit. Sol said that you are the weakest boy in the class and that she would definitely own you in an arm wrestle. You're pathetic. Insect.",
 
-  "Matt, I hear that you are from Hull. European city of culture? Don't make me laugh! ha!"
+  "I hear that you are from Hull. European city of culture? Don't make me laugh! ha!",
+
+  "you are in your early thirties and you don't know how to cook. Get a life.",
+
+  "We make jokes, but deep down I think really highly of you. Unfortunately we think you are highly irritating.",
+
+  "The only good thing I can say about you are your tight presentations. Only joking. You come across sleezy."
 ];
 var linkers = [
-    "Do you want some more?",
-    "Can you handle another you slippery fucker?",
-    "x"
+    "Mr pathetic, Do you want some more?",
+
+    "Can you handle another you slippery fuck?",
+
+    "I could go on all day....... shall I keep going. Gangly shit?",
+
+    "Want another slap, you vermin?"
 ];
 
 app.setHandler({
     'LAUNCH': function() {
-        this.ask("Welcome to Insult Matt! Matt is an interesting character. Who would like to insult him?", "Tell me your name dooch.");
+        this.ask("Welcome to Insult Matt! Who would like to insult him?", "Oi!!! Your name please.");
     },
 
     'FirstInsultIntent': function(name) {
-        this.ask("Hey Matt," + name.value + "wants to say this........." + insults[Math.floor(Math.random()*insults.length)] + ". Do you want some more?", "I'll take that as a no");
+        this.ask("Hey Matt," + name.value + "wants to say this........." + insults[Math.floor(Math.random()*insults.length)] + linkers[Math.floor(Math.random()*linkers.length)], "Oi you useless low-life. Answer me!");
     },
 
     'AMAZON.YesIntent': function(){
-        this.ask(insults[Math.floor(Math.random()*insults.length)]  + ". Do you want some more?", "I'll take that as a no");
+        this.ask(insults[Math.floor(Math.random()*insults.length)]  + linkers[Math.floor(Math.random()*linkers.length)], "I'm speaking to you sofa boy. Respond.");
+    },
+
+    'AMAZON.NoIntent': function(){
+        this.tell("Okay... slither off to bed then");
     }
 });
 
