4550d34433578a11c831ef42667144d8321f841d
DenisKent <kentdenis6@hotmail.co.uk>
2018-07-15 21:02:11 +0300

    Insult matt basic working

--- HelloWorld/app/app.js
+++ HelloWorld/app/app.js
@@ -19,7 +19,7 @@ const app = new App(config);
 var insults = [
   "Matt, you think you are full of Northern Wisdom. Riddle me this. What has girly blonde hair and is full of bull shit. Do you know the answer? It's you, you cunt!",
 
-  "Matt, or shall I call you AbuMustafa. You're not a local, shut up and sit down, Alexa is Queen.",
+  "Matt, or shall I call you AbuMustafa. You're not a local, shut up and sit down.",
 
   "Matt, you are always going on about how client facing you are. Do you want to face some hard truths? You're a cunt!",
 
@@ -29,18 +29,24 @@ var insults = [
 
   "Matt, I hear that you are from Hull. European city of culture? Don't make me laugh! ha!"
 ];
+var linkers = [
+    "Do you want some more?",
+    "Can you handle another you slippery fucker?",
+    ""
+]
+
 app.setHandler({
     'LAUNCH': function() {
-        this.toIntent('HelloWorldIntent');
-    },
-
-    'HelloWorldIntent': function() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+        this.ask("Welcome to Insult Matt! Matt is an interesting character. Who would like to insult him?", "Tell me your name dooch.");
     },
 
     'MyNameIsIntent': function(name) {
-        this.tell('Hey ' + name.value + ', nice to meet you!');
+        this.ask("Hey Matt," + name.value + "wants to say this........." + insults[Math.floor(Math.random()*insults.length)] + ". Do you want some more?", "I'll take that as a no");
     },
+
+    'AMAZON.YesIntent': function(){
+        this.ask(insults[Math.floor(Math.random()*insults.length)]  + ". Do you want some more?", "I'll take that as a no");
+    }
 });
 
 module.exports.app = app;
